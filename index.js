7f30570ca8e0325aba30146ae4057509e4ebbd35
Miriam Völkl <miriamvoelkl@gmail.com>
2023-12-18 14:06:42 +0100

    add solutions

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ import { checkFilteredNews, checkSortedNews } from "./utils/results.js";
 const container = document.querySelector('[data-js="card-container"]');
 
 // Part 1 - start here
-const filteredNews = news.filter(() => {
-  return true;
-});
+const filteredNews = news.filter((story) =>
+  story.categories.includes("politics")
+);
+
+console.log("filteredNews:", filteredNews);
 
 // Part 2 - start here
-const sortedNews = filteredNews;
+const sortedNews = filteredNews.slice().sort((a, b) => {
+  if (a.body.length < b.body.length) {
+    return -1;
+  } else if (a.body.length > b.body.length) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
 
 sortedNews.forEach((news) => {
   const cardElement = Card(news);
